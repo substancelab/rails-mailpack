f61def3641d94a352f37ab51e6af9c8e57d58363
Jakob Skjerning <jakob@mentalized.net>
2016-10-10 10:43:27 +0200

    Move gem initialization to the default location

--- lib/rails/mailpack.rb
+++ lib/rails/mailpack.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
+$:.push File.expand_path("..", __FILE__)
+
 require "rails/mailpack/version"
+
+autoload :HtmlLinksToTextInterceptor, 'html_links_to_text_interceptor'
 
 module Rails
   module Mailpack
-    # Your code goes here...
   end
 end
--- lib/rails_mailpack.rb
+++
@@ -1,2 +0,0 @@
-$:.push File.expand_path("..", __FILE__)
-autoload :HtmlLinksToTextInterceptor, 'html_links_to_text_interceptor'
